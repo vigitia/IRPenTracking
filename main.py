abd20d514e51851fd4cc9d0dc9ad949661b240e6
Vitus <vitus.maierhoefer@gmail.com>
2023-02-03 17:05:51 +0100

    Rework testing mode

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ ENABLE_UNIX_SOCKET = True
 UNIX_SOCK_NAME = 'uds_test'
 PIPE_NAME = 'pipe_test'
 
-DEBUG_MODE = False  # Enable for Debug print statements and preview windows
-SEND_TO_FRONTEND = True  # Enable if points should be forwarded to the sdl frontend
+DEBUG_MODE = 1  # Enable for Debug print statements and preview windows
+SEND_TO_FRONTEND = 0  # Enable if points should be forwarded to the sdl frontend
 
 TRAINING_DATA_COLLECTION_MODE = False  # Enable if ROIs should be saved to disk
 
@@ -30,7 +30,6 @@ if DOCUMENTS_DEMO:
 class Main:
 
     rois = []  # Regions of interest
-    active_pen_events = []
 
     uds_initialized = False
     sock = None
@@ -38,7 +37,18 @@ class Main:
 
     last_heartbeat_timestamp = 0
 
+    preview_initialized = False
+
     def __init__(self):
+
+        if SEND_TO_FRONTEND:
+            if ENABLE_FIFO_PIPE:
+                if not os.path.exists(PIPE_NAME):
+                    os.mkfifo(PIPE_NAME)
+                self.pipeout = os.open(PIPE_NAME, os.O_WRONLY)
+
+            if ENABLE_UNIX_SOCKET:
+                self.init_unix_socket()
 
         self.ir_pen = IRPen()
         self.flir_blackfly_s = FlirBlackflyS(subscriber=self)
@@ -56,6 +66,18 @@ class Main:
 
         thread = threading.Thread(target=self.main_thread)
         thread.start()
+
+    def init_unix_socket(self):
+        self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+        print('Connecting to UNIX Socket %s' % UNIX_SOCK_NAME)
+        try:
+            self.sock.connect(UNIX_SOCK_NAME)
+        except socket.error as error:
+            print('Error while connecting to UNIX Socket:', error)
+            print('Make sure that the frontend is already running before starting this python script')
+            sys.exit(1)
+        self.uds_initialized = True
 
     # ----------------------------------------------------------------------------------------------------------------
 
@@ -217,59 +239,7 @@ class Main:
                 print('Broken Pipe in delete_line()')
                 self.init_unix_socket()
 
-    # ----------------------------------------------------------------------------------------------------------------
-
-    def add_new_line_point(self):
-        if not self.uds_initialized:
-            return 0
-
-        message = 'l {} 255 255 255 {} {} {} |'.format(self.active_pen_events[0].id,
-                                                       int(self.active_pen_events[0].x),
-                                                       int(self.active_pen_events[0].y),
-                                                       0 if self.active_pen_events[0].state == PenState.HOVER else 1)
-        # print(message)
-        if ENABLE_FIFO_PIPE:
-            os.write(self.pipeout, bytes(message, 'utf8'))
-        if ENABLE_UNIX_SOCKET:
-            try:
-                self.sock.sendall(message.encode())
-            except Exception as e:
-                print(e)
-                print('---------')
-                print('Broken Pipe in add_new_line_point()')
-                self.init_unix_socket()
-
-    def append_line(self, line_id, line):
-
-        print('append', line_id, line)
-
-        if not self.uds_initialized:
-            return 0
-
-        line_color = '255 255 255'
-
-        message = f'a {line_id} {line_color} '
-
-        for i, point in enumerate(line):
-            message += f'{int(point[0])},{int(point[1])}'
-            if i < len(line) - 1:
-                message += ';'
-
-        message += ' - |'  # Append message end
-
-        print('message append', len(message), message)
-
-        if ENABLE_FIFO_PIPE:
-            os.write(self.pipeout, bytes(message, 'utf8'))
-        if ENABLE_UNIX_SOCKET:
-            try:
-                self.sock.send(message.encode())
-            except Exception as e:
-                print(e)
-                print('---------')
-                print('Broken Pipe in append_line')
-                self.init_unix_socket()
-
+    # For documents demo
     def clear_rects(self):
         if not self.uds_initialized:
             return 0
@@ -285,118 +255,132 @@ class Main:
                 print('Broken Pipe in clear_rects()')
                 self.init_unix_socket()
 
-    def init_unix_socket(self):
-        self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    # ----------------------------------------------------------------------------------------------------------------
 
-        print('Connecting to UNIX Socket %s' % UNIX_SOCK_NAME)
-        try:
-            self.sock.connect(UNIX_SOCK_NAME)
-        except socket.error as msg:
-            print(msg)
-            sys.exit(1)
-        self.uds_initialized = True
+    def add_new_line_point(self, active_pen_event):
+        if not self.uds_initialized:
+            print('Error in add_new_line_point(): uds not initialized')
+            return False
+
+        message = 'l {} 255 255 255 {} {} {} |'.format(active_pen_event.id,
+                                                       int(active_pen_event.x),
+                                                       int(active_pen_event.y),
+                                                       0 if active_pen_event.state == PenState.HOVER else 1)
+
+        if ENABLE_FIFO_PIPE:
+            os.write(self.pipeout, bytes(message, 'utf8'))
+        if ENABLE_UNIX_SOCKET:
+            try:
+                self.sock.sendall(message.encode())
+                print('send line point')
+            except Exception as e:
+                print(e)
+                print('---------')
+                print('Broken Pipe in add_new_line_point()')
+                self.init_unix_socket()
+
+    # def append_line(self, line_id, line):
+    #
+    #     print('append', line_id, line)
+    #
+    #     if not self.uds_initialized:
+    #         return 0
+    #
+    #     line_color = '255 255 255'
+    #
+    #     message = f'a {line_id} {line_color} '
+    #
+    #     for i, point in enumerate(line):
+    #         message += f'{int(point[0])},{int(point[1])}'
+    #         if i < len(line) - 1:
+    #             message += ';'
+    #
+    #     message += ' - |'  # Append message end
+    #
+    #     print('message append', len(message), message)
+    #
+    #     if ENABLE_FIFO_PIPE:
+    #         os.write(self.pipeout, bytes(message, 'utf8'))
+    #     if ENABLE_UNIX_SOCKET:
+    #         try:
+    #             self.sock.send(message.encode())
+    #         except Exception as e:
+    #             print(e)
+    #             print('---------')
+    #             print('Broken Pipe in append_line')
+    #             self.init_unix_socket()
 
     def main_thread(self):
-        if ENABLE_FIFO_PIPE:
-            if not os.path.exists(PIPE_NAME):
-                os.mkfifo(PIPE_NAME)
-            self.pipeout = os.open(PIPE_NAME, os.O_WRONLY)
-
-        if ENABLE_UNIX_SOCKET:
-            self.init_unix_socket()
-
-        if DEBUG_MODE:
-            cv2.namedWindow('ROI', cv2.WINDOW_NORMAL)
-            cv2.resizeWindow('ROI', 1000, 500)
-
         while True:
-            if SEND_TO_FRONTEND:
-                if len(self.active_pen_events) > 0:
-                    self.add_new_line_point()
-                    # Reset list because we do not want to send the same points again
-                    self.active_pen_events = []
-
-                # for line_id, line in self.remaining_line_points.items():
-                #     self.append_line(line_id, line)
-                # # Reset because we do not want to send it again
-                # self.remaining_line_points = {}
-
-                if not DEBUG_MODE:
-                    # TODO: Check why we need a delay here. Without it, it will lag horribly.
-                    #  time.sleep() does not work here
-                    cv2.waitKey(1)
 
             if DEBUG_MODE:
-                if len(self.rois) == 2:
-                    roi0 = cv2.resize(self.rois[0], (500, 500), interpolation=cv2.INTER_AREA)
-                    max0 = str(np.max(roi0))
-                    roi1 = cv2.resize(self.rois[1], (500, 500), interpolation=cv2.INTER_AREA)
-                    max1 = str(np.max(roi1))
-
-                    roi0 = cv2.putText(
-                        img=roi0,
-                        text=max0,
-                        org=(50, 50),
-                        fontFace=cv2.FONT_HERSHEY_DUPLEX,
-                        fontScale=3.0,
-                        color=(255),
-                        thickness=3
-                    )
-
-                    roi1 = cv2.putText(
-                        img=roi1,
-                        text=max1,
-                        org=(50, 50),
-                        fontFace=cv2.FONT_HERSHEY_DUPLEX,
-                        fontScale=3.0,
-                        color=(255),
-                        thickness=3
-                    )
-
-                    cv2.imshow('ROI', cv2.hconcat([roi0, roi1]))
-                    self.rois = []
-                elif len(self.rois) == 1:
-                    roi0 = cv2.resize(self.rois[0], (500, 500), interpolation=cv2.INTER_AREA)
-                    max0 = str(np.max(roi0))
-
-                    roi0 = cv2.putText(
-                        img=roi0,
-                        text=max0,
-                        org=(50, 50),
-                        fontFace=cv2.FONT_HERSHEY_DUPLEX,
-                        fontScale=3.0,
-                        color=(255),
-                        thickness=3
-                    )
-                    roi1 = np.zeros((500, 500), np.uint8)
-                    cv2.imshow('ROI', cv2.hconcat([roi0, roi1]))
-                    self.rois = []
+                self.show_debug_preview()
 
                 key = cv2.waitKey(1)
                 if key == 27:  # ESC
                     cv2.destroyAllWindows()
                     sys.exit(0)
+            else:
+                # TODO: Check why we need a delay here. Without it, it will lag horribly.
+                #  time.sleep() does not work here
+                cv2.waitKey(1)
+
+    def show_debug_preview(self):
+
+        # TODO: Label origin camera
+
+        if not self.preview_initialized:
+            cv2.namedWindow('ROI', cv2.WINDOW_NORMAL)
+            cv2.resizeWindow('ROI', 1920, 960)
+            self.preview_initialized = True
+
+        if len(self.rois) == 2:
+            roi0 = cv2.resize(self.rois[0], (960, 960), interpolation=cv2.INTER_AREA)
+            roi1 = cv2.resize(self.rois[1], (960, 960), interpolation=cv2.INTER_AREA)
+
+            roi0 = self.add_max_brightness_label(roi0, np.max(roi0))
+            roi1 = self.add_max_brightness_label(roi1, np.max(roi1))
+
+            cv2.imshow('ROI', cv2.hconcat([roi0, roi1]))
+
+        elif len(self.rois) == 1:
+            roi0 = cv2.resize(self.rois[0], (960, 960), interpolation=cv2.INTER_AREA)
+            roi1 = np.zeros((960, 960), np.uint8)
+
+            roi0 = self.add_max_brightness_label(roi0, np.max(roi0))
+
+            cv2.imshow('ROI', cv2.hconcat([roi0, roi1]))
+
+        self.rois = []
+
+    def add_max_brightness_label(self, frame, max_brightness):
+        return cv2.putText(
+            img=frame,
+            text=str(max_brightness),
+            org=(90, 90),
+            fontFace=cv2.FONT_HERSHEY_DUPLEX,
+            fontScale=2.0,
+            color=(255),
+            thickness=3
+        )
+
 
     def on_new_frame_group(self, frames, camera_serial_numbers, matrices):
         if len(frames) > 0:
             if TRAINING_DATA_COLLECTION_MODE:
                 self.training_images_collector.save_training_images(frames)
             else:
-                active_pen_events, stored_lines, _, _, debug_distances, rois = self.ir_pen.get_ir_pen_events_multicam(
-                    frames, matrices)
+                active_pen_events, stored_lines, _, _, debug_distances, rois = self.ir_pen.get_ir_pen_events(frames, matrices)
 
-                self.active_pen_events = active_pen_events
+                if SEND_TO_FRONTEND:
+                    for active_pen_event in active_pen_events:
+                        self.add_new_line_point(active_pen_event)
+
                 self.rois = rois
 
                 if DOCUMENTS_DEMO:
                     self.analogue_digital_document.on_new_finished_lines(stored_lines)
-                    # line_ids_to_delete, remaining_line_points = self.analogue_digital_document.on_new_finished_lines(stored_lines)
-                    # if len(line_ids_to_delete) > 0:
-                    #     for line_id in line_ids_to_delete:
-                    #         self.delete_line(line_id)
-                    #
-                    # self.remaining_line_points = remaining_line_points
 
 
 if __name__ == '__main__':
-    debugger = Main()
+    main = Main()
