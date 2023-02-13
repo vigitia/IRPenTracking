972754f83a7f0f8bf9892c91c0e15378b2c104ef
Vitus <vitus.maierhoefer@gmail.com>
2023-02-13 19:00:57 +0100

    Improve uds test script

--- sdl_frontend/uds_multipointer.py
+++ sdl_frontend/uds_multipointer.py
@@ -8,7 +8,7 @@ import random
 sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = '/tmp/sock.uds'
+server_address = '../uds_test'
 print('connecting to %s' % server_address)
 try:
     sock.connect(server_address)
@@ -32,9 +32,9 @@ except socket.error as msg:
 counter = 0
 
 x_min = 0
-x_max = 1920
+x_max = 3840
 y_min = 0
-y_max = 1080
+y_max = 2160
 line_id_1 = 0
 line_id_2 = 1
 num_points_1 = 0
@@ -52,7 +52,7 @@ color_1 = (255, 255, 255)
 color_2 = (255, 255, 255)
 
 while True:
-    time.sleep(0.1)
+    time.sleep(0.01)
     #time.sleep(0.01)
     num_points_1 += 1
     x_1 += random.randint(-delta, delta)
@@ -68,9 +68,6 @@ while True:
     elif y_1 > y_max:
         y_1 = y_max
 
-    sock.send(f'l {line_id_1} {color_1[0]} {color_1[1]} {color_1[2]} {x_1} {y_1} {state_1} |'.encode())
-    print(f'{line_id_1} {x_1} {y_1}')
-
     num_points_2 += 1
     x_2 += random.randint(-delta, delta)
     y_2 += random.randint(-delta, delta)
@@ -85,25 +82,28 @@ while True:
     elif y_2 > y_max:
         y_2 = y_max
 
+    sock.send(f'l {line_id_1} {color_1[0]} {color_1[1]} {color_1[2]} {x_1} {y_1} {state_1} |'.encode())
     sock.send(f'l {line_id_2} {color_2[0]} {color_2[1]} {color_2[2]} {x_2} {y_2} {state_2} |'.encode())
+
     print(f'{line_id_2} {x_2} {y_2}')
+    print(f'{line_id_1} {x_1} {y_1}')
 
     #time.sleep(0.1)
 
     if num_points_1 > 2:
-        if random.randint(0, 20) == 0:
+        if random.randint(0, 100) == 0:
             sock.send(f'l {line_id_1} {color_1[0]} {color_1[1]} {color_1[2]} {x_1} {y_1} 0 |'.encode())
             sock.send(f'f {line_id_1} |'.encode())
-            line_id_1 += 2
+            line_id_1 += 1
             num_points_1 = 0
             state_1 = random.randint(0, 1)
             color_1 = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
     if num_points_2 > 2:
-        if random.randint(0, 20) == 0:
+        if random.randint(0, 100) == 0:
             sock.send(f'l {line_id_2} {color_2[0]} {color_2[1]} {color_2[2]} {x_2} {y_2} 0 |'.encode())
             sock.send(f'f {line_id_2} |'.encode())
-            line_id_2 += 2
+            line_id_2 += 1
             num_points_2 = 0
             state_2 = random.randint(0, 1)
             color_2 = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
