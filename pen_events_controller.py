f5c77a944879480fcde90486ce00e096ba81c6a1
Vitus <vitus.maierhoefer@gmail.com>
2023-02-14 14:19:47 +0100

    Fix multiple bugs in processing logic of PenEvents

--- pen_events_controller.py
+++ pen_events_controller.py
@@ -1,24 +1,26 @@
+
 import time
 import numpy as np
-from scipy.spatial import distance
 
+from scipy.spatial import distance
 from pen_state import PenState
 
 
 # Hover will be selected over Draw if Hover Event is within the last X event states.
 # Enable this if too many unwanted short lines appear while drawing
-HOVER_WINS = False
-NUM_HOVER_EVENTS_TO_END_LINE = 6  # We expect at least X Hover Events in a row to end that pen event
-NUM_CHECK_LAST_EVENT_STATES = 3  # Check the last X Pen Events if they contain a hover event
+# HOVER_WINS = False
+# NUM_CHECK_LAST_EVENT_STATES = 3  # Check the last X Pen Events if they contain a hover event
+
+NUM_HOVER_EVENTS_TO_END_LINE = 5  # We expect at least X Hover Events in a row to end that pen event
 
 # Amount of time a pen can be missing until the pen event will be ended
-TIME_POINT_MISSING_THRESHOLD_MS = 30 # 15
+TIME_POINT_MISSING_THRESHOLD_MS = 60  # 15
 
-MAX_DISTANCE_FOR_MERGE = 200  # Maximum Distance between two points for them to be able to merge
+MAX_DISTANCE_FOR_MERGE = 500  # 200  # Maximum Distance between two points for them to be able to merge
 
 # Simple Smoothing of the output points. 1 -> No smoothing; 0.5 -> Calculate a new point and use 50% of the previous
-# points and 50% of the new points location.
-SMOOTHING_FACTOR = 0.2  # Value between 0 and 1, depending on if the old or the new value should count more.
+# point's and 50% of the new point's location.
+SMOOTHING_FACTOR = 1  # 0.2  # Value between 0 and 1, depending on if the old or the new value should count more.
 
 DEBUG_MODE = False
 
@@ -31,14 +33,11 @@ class PenEventsController:
 
     # If a pen event is over, it will be deleted. But its points will be stored here to keep track of all drawn lines.
     stored_lines = []
-    # pen_events_to_remove = []  # Points that got deleted from active_points in the current frame
 
     highest_id = 0  # Assign each new pen event a new id. This variable keeps track of the highest number.
 
     def merge_pen_events_new(self, new_pen_events):
-
-        # TODO: Global or not?
-        pen_events_to_remove = []
+        pen_events_to_remove = []  # Points that got deleted from active_points in the current frame
 
         now = round(time.time() * 1000)  # Get current timestamp
 
@@ -47,12 +46,13 @@ class PenEventsController:
 
             remaining_events = []
 
+            # Remove events that are missing for too long
             for active_pen_event in self.active_pen_events:
                 # print('Remaining active event but now new event to merge with')
                 time_since_last_seen = now - active_pen_event.last_seen_timestamp
 
                 if time_since_last_seen > TIME_POINT_MISSING_THRESHOLD_MS:
-                    print('PEN Event {} ({} points) gets deleted due to inactivity with state {}'.format(active_pen_event.id, len(active_pen_event.history), active_pen_event.state))
+                    print('Delete PenEvent {} ({} points): Inactive with last state {}'.format(active_pen_event.id, len(active_pen_event.history), active_pen_event.state))
                     if len(active_pen_event.history) > 0:
                         # self.stored_lines.append(np.array(active_pen_event.history))
                         self.stored_lines.append({active_pen_event.id: active_pen_event.history})
@@ -68,8 +68,9 @@ class PenEventsController:
 
         # Merge new and old
         # Compare all new_pen_events and active_pen_events and pair them by shortest distance to each other
-        shortest_distance_point_pairs = self.calculate_distances_between_all_points(self.active_pen_events, new_pen_events)
+        shortest_distance_point_pairs = self.__calculate_distances_between_all_points(self.active_pen_events, new_pen_events)
 
+        # This is not the event ID but the index pos in the list
         merged_ids_active_pen_events = []
         merged_ids_new_pen_events = []
 
@@ -86,36 +87,18 @@ class PenEventsController:
             if distance_between_points <= MAX_DISTANCE_FOR_MERGE:
 
                 # Merge events
-                new_pen_event.id = active_pen_event.id
-                new_pen_event.first_appearance = active_pen_event.first_appearance
-                new_pen_event.state_history = active_pen_event.state_history
-                new_pen_event.state_history.append(new_pen_event.state)
-                new_pen_event.last_seen_timestamp = now
-                new_pen_event.history = active_pen_event.history
+                merged_event = self.__merge_events(active_pen_event, new_pen_event, now,
+                                                   self.active_pen_events, new_pen_events)
+                final_pen_events.append(merged_event)
 
-                # Apply smoothing to the points by taking their previous positions into account
-                new_pen_event.x = SMOOTHING_FACTOR * (new_pen_event.x - active_pen_event.x) + active_pen_event.x
-                new_pen_event.y = SMOOTHING_FACTOR * (new_pen_event.y - active_pen_event.y) + active_pen_event.y
-
-                new_pen_event.history.append((new_pen_event.x, new_pen_event.y))
-
-                final_pen_events.append(new_pen_event)
                 merged_ids_active_pen_events.append(entry[0])
                 merged_ids_new_pen_events.append(entry[1])
 
         # Iterate over all remaining events in both active_pen_events and new_pen_events
         # Add remaining new_pen_events directly to final_pen_events
         # Check remaining active_pen_events if they are still relevant
-
         for i, new_pen_event in enumerate(new_pen_events):
             if i not in merged_ids_new_pen_events:
-
-                # Add current position to the history list, but ignore hover events
-                if new_pen_event.state != PenState.HOVER:
-                    new_pen_event.history.append((new_pen_event.x, new_pen_event.y))
-
-                new_pen_event.state_history.append(new_pen_event.state)
-                new_pen_event.last_seen_timestamp = now
                 final_pen_events.append(new_pen_event)
 
         for i, active_pen_event in enumerate(self.active_pen_events):
@@ -123,7 +106,7 @@ class PenEventsController:
                 time_since_last_seen = now - active_pen_event.last_seen_timestamp
 
                 if time_since_last_seen > TIME_POINT_MISSING_THRESHOLD_MS:
-                    print('PEN Event {} ({} points) gets deleted due to inactivity with state {}'.format(
+                    print('Delete PenEvent {} ({} points): Inactive with last state {}'.format(
                         active_pen_event.id, len(active_pen_event.history), active_pen_event.state))
                     if len(active_pen_event.history) > 0:
                         # self.stored_lines.append(np.array(active_pen_event.history))
@@ -141,21 +124,17 @@ class PenEventsController:
 
             # Check if there are too many hover events. End event if this is the case
             if len(final_pen_event.history) >= 1 and len(final_pen_event.state_history) >= NUM_HOVER_EVENTS_TO_END_LINE:
-                if final_pen_event.state_history[-NUM_HOVER_EVENTS_TO_END_LINE:].count(PenState.HOVER) == NUM_HOVER_EVENTS_TO_END_LINE:
-                    # if DEBUG_MODE:
-                    # print('Pen Event {} turned from State.DRAG into State.HOVER'.format(final_pen_event.id))
-                    if len(final_pen_event.history) > 0:
-                        # self.stored_lines.append(np.array(final_pen_event.history))
+                # There needs to be at least one Draw event to delete a line (except through inactivity)
+                if PenState.DRAG in final_pen_event.state_history:
+                    if final_pen_event.state_history[-NUM_HOVER_EVENTS_TO_END_LINE:].count(PenState.HOVER) == NUM_HOVER_EVENTS_TO_END_LINE:
+
+                        final_pen_event.history.append((final_pen_event.x, final_pen_event.y))
                         self.stored_lines.append({final_pen_event.id: final_pen_event.history})
                         pen_events_to_remove.append(final_pen_event)
-                        # TODO: remove event from final_pen_events
 
-                    # print(final_pen_event.state, final_pen_event.state_history[-5:])
-                    # if DEBUG_MODE:
-                    print('PEN Event {} ({} points) gets deleted because DRAW ended'.format(final_pen_event.id,
-                                                                                            len(final_pen_event.history)))
+                        print('Delete PenEvent {} ({} points): DRAW ended'.format(final_pen_event.id, len(final_pen_event.history)))
 
-                    continue
+                        continue
 
             final_final_pen_events.append(final_pen_event)
 
@@ -163,9 +142,55 @@ class PenEventsController:
 
         return self.active_pen_events, self.stored_lines, pen_events_to_remove
 
+    # Combine a PenEvent from the previous frame with a new PenEvent from the current frame
+    def __merge_events(self, active_pen_event, new_pen_event, current_timestamp, active_pen_events, new_pen_events):
+        if (distance.euclidean(new_pen_event.get_coordinates(), active_pen_event.get_coordinates())) > 200:
+            print('Dist')
 
+        new_pen_event.id = active_pen_event.id  # Use existing ID
+        new_pen_event.last_seen_timestamp = current_timestamp
+        new_pen_event.first_appearance = active_pen_event.first_appearance
 
+        new_pen_event.history = active_pen_event.history  # Transfer over history
+        new_pen_event.history.append((active_pen_event.x, active_pen_event.y))  # Add the last point from the old event
 
+        new_pen_event.state_history = active_pen_event.state_history  # Transfer over state history
+        new_pen_event.state_history.append(active_pen_event.state)  # Add last state from old PenEvent
+
+        # Apply smoothing to the points by taking their previous positions into account
+        new_pen_event.x = SMOOTHING_FACTOR * (new_pen_event.x - active_pen_event.x) + active_pen_event.x
+        new_pen_event.y = SMOOTHING_FACTOR * (new_pen_event.y - active_pen_event.y) + active_pen_event.y
+
+        return new_pen_event
+
+    def __assign_new_ids(self, new_pen_events):
+        final_pen_events = []
+
+        for new_pen_event in new_pen_events:
+            if new_pen_event.id == -1:
+                new_pen_event.id = self.highest_id
+                print('New PenEvent {}'.format(self.highest_id))
+                self.highest_id += 1
+            final_pen_events.append(new_pen_event)
+        return final_pen_events
+
+    def __calculate_distances_between_all_points(self, point_list_one, point_list_two):
+        distances = []
+
+        for i in range(len(point_list_one)):
+            for j in range(len(point_list_two)):
+                distance_between_points = distance.euclidean(point_list_one[i].get_coordinates(),
+                                                             point_list_two[j].get_coordinates())
+                distances.append([i, j, distance_between_points])
+
+        # Sort list of lists by third element, in this case the distance between the points
+        # https://stackoverflow.com/questions/4174941/how-to-sort-a-list-of-lists-by-a-specific-index-of-the-inner-list
+        distances.sort(key=lambda x: x[2])
+
+        return distances
+
+    # ------------------------------------------------------------------------------------------------------------------
+    # ------------------------------------------------------------------------------------------------------------------
 
 
     # Simplified function when only one pen can be present at the time
@@ -291,19 +316,11 @@ class PenEventsController:
 
         return self.active_pen_events, self.stored_lines, self.pen_events_to_remove
 
-    def __assign_new_ids(self, new_pen_events):
-        final_pen_events = []
+    # Assign a new ID to all PenEvents that have the placeholder ID "-1"
 
-        for new_pen_event in new_pen_events:
-            if new_pen_event.id == -1:
-                new_pen_event.id = self.highest_id
-                print('Assigned ID {} to a new Pen Event'.format(self.highest_id))
-                self.highest_id += 1
-            final_pen_events.append(new_pen_event)
-        return final_pen_events
 
     # Function to merge the events, even when multiple pens are present at the same time
-    def merge_pen_events_multiple(self, new_pen_events):
+    def merge_pen_events_multiple_old(self, new_pen_events):
         now = round(time.time() * 1000)  # Get current timestamp
 
         # # Keep track of the current state
@@ -318,8 +335,8 @@ class PenEventsController:
                 self.__process_click_events(active_pen_event)
 
         # Compare all new_pen_events and active_pen_events and pair them by shortest distance to each other
-        shortest_distance_point_pairs = self.calculate_distances_between_all_points(self.active_pen_events,
-                                                                                    new_pen_events, as_objects=True)
+        shortest_distance_point_pairs = self.__calculate_distances_between_all_points(self.active_pen_events,
+                                                                                      new_pen_events, as_objects=True)
 
         for entry in shortest_distance_point_pairs:
             last_pen_event = self.active_pen_events[entry[0]]
@@ -467,7 +484,7 @@ class PenEventsController:
 
         return final_pen_events
 
-    def __process_click_events(self, active_pen_event):
+    def __process_click_events_old(self, active_pen_event):
         # Check if click event happens without too much movement
         xs = []
         ys = []
@@ -500,25 +517,5 @@ class PenEventsController:
         self.pen_events_to_remove.append(active_pen_event)
         self.active_pen_events.remove(active_pen_event)
 
-    def calculate_distances_between_all_points(self, point_list_one, point_list_two):
-        distances = []
 
-        for i in range(len(point_list_one)):
-            for j in range(len(point_list_two)):
-                distance_between_points = distance.euclidean(point_list_one[i].get_coordinates(),
-                                                                point_list_two[j].get_coordinates())
-
-                # # if distance_between_points > max_distance:
-                # if distance_between_points > max_distance:
-                #     print('Distance {} TOO LARGE'.format(distance_between_points))
-                #     continue
-                # else:
-                #     print('Distance:', distance_between_points)
-                distances.append([i, j, distance_between_points])
-
-        # Sort list of lists by third element, in this case the distance between the points
-        # https://stackoverflow.com/questions/4174941/how-to-sort-a-list-of-lists-by-a-specific-index-of-the-inner-list
-        distances.sort(key=lambda x: x[2])
-
-        return distances
 
