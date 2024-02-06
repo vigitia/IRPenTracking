aefc4a93687fde71c4bef675a5de7de9d2cb813c
VitusMa <vitus.maierhoefer@gmail.com>
2024-02-06 18:51:27 +0100

    Add entries to constants file

--- Constants.py
+++ Constants.py
@@ -5,3 +5,7 @@ OUTPUT_WINDOW_HEIGHT = 1080 #2160
 # Width and height of the received frames
 INPUT_FRAME_WIDTH = 1920  # 848
 INPUT_FRAME_HEIGHT = 1200  # 480
+
+# For Flir Blackfly S Calibration
+CAM_EXPOSURE_FOR_CALIBRATION = 150000  # Increase Brightness to better see the corners
+CAM_GAIN_FOR_CALIBRATION = 150  # Increase Brightness to better see the corners
